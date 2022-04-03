3353a72d21c0f28a703a1e199ee166cfa9122c4b
VidyaBhushan Mehta <vidyabhushanmehta@vibhu.local>
2022-04-03 11:48:41 +0530

    test file name updated

--- inbox-updated/test/Inbox.test.js
+++ inbox-updated/test/Inbox.test.js
@@ -1,31 +1,43 @@
-// const assert = require('assert');
-// const ganache = require('ganache-cli');
-// const Web3 = require('web3');
-// //Here Web3 provider is ganache, just for testing on test env.
-// const web3 = new Web3(ganache.provider());
-//
-// class Car{
-//   park(){
-//     return 'stopped';
-//   }
-//
-//   drive(){
-//     return 'vroom';
-//   }
-// }
-//
-// let car;
-//
-// beforeEach(() =>{
-//   car = new Car();
-// });
-//
-// describe('Car', () => {
-//     it('can park', () =>{
-//       assert.equal(car.park(), 'stopped');
-//     });
-//
-//     it('can drive', ()=>{
-//       assert.equal(car.drive(), 'vroom');
-//     });
-// });
+//=========********************========================
+//  Deployment with ganache provider
+//=========********************========================
+
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+//const {interface, bytecode} = require('../compile');
+
+const { abi, evm } = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach( async () => {
+    // Get list of all accounts
+    accounts = await web3.eth.getAccounts();
+
+    //Use one of the accounts to deploy the contract
+    inbox = await new web3.eth.Contract(abi)
+      .deploy({ data: evm.bytecode.object, arguments:['Hi There!!']})
+      .send({ from : accounts[0], gas:'1000000'});
+});
+
+describe('Inbox', ()=>{
+  it('deploys a contract', ()=>{
+    assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async ()=>{
+      const message = await inbox.methods.message().call();
+      assert.equal(message,'Hi There!!');
+  });
+
+  it('can change the message', async ()=>{
+    await inbox.methods.setMessage('bye').send( {from:accounts[0]} );
+    const message = await inbox.methods.message().call();
+    assert.equal(message,'bye');
+  });
+
+});
